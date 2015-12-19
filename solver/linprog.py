2216b3c7b38225703d70d7bb608e879bcf12d938
Matthijs Moed <m.h.moed@lumc.nl>
2015-12-19 13:18:53 +0100

    Added some more documentation to LP solution algorithm.

--- solver/linprog.py
+++ solver/linprog.py
@@ -1,6 +1,41 @@
 """
 Implement jigsaw puzzle solver from Yu et al. (2015).
 
+An LP is constructed based on the current active selection and weights
+lookup table. The objective function to be minimised is the sum of the
+number of mismatches in the x direction, plus the number of mismatches in
+the y direction. Mismatches are multiplied by their MGC distance.
+
+The linear program formulated in Yu et al. (2015) is as follows:
+
+    minimise   sum_{for all (i, j, o) in A} w_ijo * h_ijo
+    subject to h_ijo >= x_i  - x_j - delta_o^x
+               h_ijo >= -x_i + x_j + delta_o^x
+
+where h_ijo are auxilliary variables introduced to solve for x, and
+delta_o^x the x-offsets for an oriented pair (i, j, o), where i, j, and o
+are the index of piece i, piece j and orientation o. Furthermore, w_ijo
+is the matching weight for the oriented pair (i, j, o).
+Recall that a matching weight is large when the MGC distance between piece i
+and j for orientation o is small (i.e. a good match), and a matching weight is
+small when the MGC distance is large. Therefore, the quickest reduction in
+the cost of the objective function is to assign h_ijo to zero for the largest
+w_ijo, i.e. to set h_ijo's to zero for the best matches. The only way in which
+to set h_ijo to zero, is to ensure pieces i and j are adjoining in a correct
+fashion. For example, with x-coordinates set to 3 and 4 to piece i and j, and
+the orientation set to 2 (j to the right of i), the inequality constraints for
+oriented pair (3, 4, 2) are as follows:
+
+    h_3,4,2 >= 3 - 4 + 1
+    h_3,4,2 >= -3 + 4 -1
+
+Given these constraints, h_3,4,2 can be set to 0, and the cost of the objective
+function reduced proportionally to w_3,4,2.
+
+To summarise, the LP's objective function is minimised by assigning pieces in
+such a way that best matches are placed in the correct orientation relative to
+each other.
+
 References:
     Yu, R., Russell, C., & Agapito, L. (2015). Solving Jigsaw Puzzles with
     Linear Programming. arXiv preprint arXiv:1511.04472.
@@ -83,10 +118,9 @@ def compute_solution(active_selection, weights, maxiter=None):
     Compute the solution of a linear program (LP) given by the active selection
     and weights lookup table.
 
-    An LP is constructed based on the current active selection and weights
-    lookup table. The objective function to be minimised is the sum of the
-    number of mismatches in the x direction, plus the number of mismatches in
-    the y direction. Mismatches are multiplied by their MGC distance.
+    The method constructs the inequality constraints matrix A_ub by first
+    sorting the active selection by piece i and orientation. The resulting sort
+    order simplifies the construction of A_ub.
 
     For a detailed overview, see Yu et al. (2015), equation 16.
 
@@ -97,21 +131,53 @@ def compute_solution(active_selection, weights, maxiter=None):
     :return: (x, y) tuple with x and y coordinates.
     """
     def sorted_by_i_and_o(active_selection):
+        """
+        Return the active selection, sorted by piece i, and then orientation o.
+        """
         return sorted(active_selection, key=lambda (i, _, o): (i, o))
 
     def row_index(i, o):
-        return 8 * i + NUM_CONSTRAINTS * o
+        """
+        Return the row index of the start of the constraints section for piece
+        i and orientation o.
+
+        :param i: the index of piece i.
+        :param o: the orientation.
+        :return: row index for piece i and orientation o.
+        """
+        return (slv.MGC_NUM_ORIENTATIONS * NUM_CONSTRAINTS * i) + \
+               (NUM_CONSTRAINTS * o)
 
     # Sort active selection by i and o. The resulting order allows for
-    # simplifications on the upper bound matrix A_ub.
+    # simplifications on the inequality constraint matrix A_ub.
 
     n = len(active_selection) / slv.MGC_NUM_ORIENTATIONS
     sorted_a = sorted_by_i_and_o(active_selection)
 
-    # Construct A_ub, given as follows:
+    # Construct inequality constraints matrix A_ub, given as follows:
     #    A_ub = | H1 | 0  | X |
     #           | 0  | H2 | Y |,
     # and where X = Y and H1 = H2 (constraints are identical for X and Y).
+
+    # Recall that the inequality constraints are given as:
+    #
+    #   h_ijo >= x_i  - x_j - delta_o^x
+    #   h_ijo >= -x_i + x_j + delta_o^x
+    #
+    # -delta_o^x and delta_o^x are the only constants and will be assigned to
+    # the upper bounds of the inequality constraints. Therefore, the
+    # constraints above are rewritten as follows:
+    #
+    #  -x_i + x_j + h_ijo >= -delta_o^x
+    #   x_i - x_j + h_ijo >=  delta_o^x
+    #
+    # Rewriting than greater-than-or-equal signs to smaller-than-or-equal gives:
+    #
+    #   x_i - x_j - h_ijo <=  delta_o^x
+    #  -x_i + x_j - h_ijo <= -delta_o^x
+    #
+    # Given these constraints, submatrices H1 and H2 are composed of two -1's
+    # in each column:
 
     h_base = np.array([-1] * NUM_CONSTRAINTS + [0] * (slv.MGC_NUM_ORIENTATIONS *
                                                       NUM_CONSTRAINTS *
@@ -121,11 +187,28 @@ def compute_solution(active_selection, weights, maxiter=None):
                                                     NUM_CONSTRAINTS * n,
                                                     NUM_CONSTRAINTS)]).T
 
+    # Submatrix X is composed of all (1, -1) pairs (for x_i and -x_i, resp.),
+    # and (-1, 1) pairs for all (-x_j, x_j) in the inequality constraints.
+    # Because of the fact that the active selection is sorted, X_i takes the
+    # following form:
+    #
+    # | 1  0 0 0 0 0 ... |
+    # | -1 0 0 0 0 0 ... |
+    # | 0  1 0 0 0 0 ... |
+    # | 0 -1 0 0 0 0 ... |
+    # | 0  0 0 0 0 0 ... |
+    # | ................ |
+    # | 0 0 0 0 0 0 0 1  |
+    # | 0 0 0 0 0 0 0 -1 |
+
     xi_base = np.array([1, -1] * slv.MGC_NUM_ORIENTATIONS + [0] *
                        (slv.MGC_NUM_ORIENTATIONS * NUM_CONSTRAINTS) * (n - 1))
     Xi = np.array([np.roll(xi_base, k) for k in
                    range(0, slv.MGC_NUM_ORIENTATIONS * NUM_CONSTRAINTS * n,
                          NUM_CONSTRAINTS * slv.MGC_NUM_ORIENTATIONS)]).T
+
+    # X_j is built dynamically depending on the values of the x_j's for the
+    # oriented pairs (i, j, o).
 
     Xj = np.zeros(Xi.shape, dtype=np.int32)
     for i, j, o in sorted_a:
@@ -145,7 +228,11 @@ def compute_solution(active_selection, weights, maxiter=None):
     A_ub = np.hstack([A_ub, np.vstack([X, Z_x])])
     A_ub = np.hstack([A_ub, np.vstack([Z_x, X])])
 
-    # Construct b_ub
+    # Construct the upper bounds vector b_ub for the inequality constraints
+    # matrix A_ub. This vector is given as follows:
+    #
+    # b_ub = [delta_o^x, -delta_o^x, delta_o^x, -delta_o^x, ....] +
+    # [delta_o^y, -delta_o^y, delta_o^y, -delta_o^y, ...]
 
     b_x = list(chain.from_iterable([[DELTA_X[o], -DELTA_X[o]]
                                     for (_, _, o) in sorted_a]))
@@ -153,10 +240,13 @@ def compute_solution(active_selection, weights, maxiter=None):
                                     for (_, _, o) in sorted_a]))
     b_ub = np.array(b_x + b_y)
 
-    # Construct c
+    # Construct coefficients vector c, to be used in the objective function. It
+    # is given as:
+    #
+    # c = [w_ijo for all (i, j, o) in active selection]
 
-    c_base = [weights[_] for _ in active_selection]
-    c = np.array(c_base * 2 + ([0] * 2 * n))
+    c_base = [weights[_] for _ in sorted_a]
+    c = np.array(c_base * NUM_CONSTRAINTS + ([0] * NUM_CONSTRAINTS * n))
 
     # Calculate solution
 
@@ -225,10 +315,12 @@ def solve(images, maxiter=None, random_seed=None):
 
     old_x, old_y = None, None
 
-    while (old_x is None and old_y is None) or not (np.array_equal(old_x, x) and np.array_equal(old_y, y)):
+    while (old_x is None and old_y is None) or not \
+            (np.array_equal(old_x, x) and np.array_equal(old_y, y)):
         rejected_matches = compute_rejected_matches(active_selection, x, y)
         pairwise_matches = list(set(pairwise_matches) - rejected_matches)
-        active_selection = compute_active_selection(pairwise_matches, mgc_distances)
+        active_selection = compute_active_selection(pairwise_matches,
+                                                    mgc_distances)
 
         old_x, old_y = x, y
         x, y = compute_solution(active_selection, weights, maxiter)
